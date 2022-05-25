379d417f1a61f8d5c60655e690213e9a61b3fa39
Myla <Myla@DESKTOP-V9A76BG>
2022-05-25 20:14:10 -0300

    Add project files.

---
+++ Exercicio-04/Exercicio-04/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int anos = 0;
+int meses = 0;
+int dias = 0;
+int resultado = 0;
+
+Console.WriteLine("Quantos anos você tem?");
+anos = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantos meses de idade você tem?");
+meses = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Quantos dias de idade você tem?");
+dias = Convert.ToInt32(Console.ReadLine());
+
+resultado = (anos * 365) + (meses * 30) + dias;
+Console.WriteLine("Você já viveu " + resultado + " dias!");
+
+Console.ReadLine();
